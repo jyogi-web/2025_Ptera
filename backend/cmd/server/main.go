728e88077df1cbdc30e1947dc31e1f38f9e01a72
KOU050223 <kouhei20050223@icloud.com>
2025-12-16 21:25:20 +0900

    feat: Dockerを用いたGo gRPCサービスの初期設定を実装

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	listenPort := 50051
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", listenPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+
+	fmt.Printf("server listening at %v\n", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
