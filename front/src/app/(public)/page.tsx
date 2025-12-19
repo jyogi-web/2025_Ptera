006b2a45dacb1a4f771f90b66467f4a28f6f9a51
Yanai1005 <tatsuya871_1005@icloud.com>
2025-12-20 04:43:40 +0900

    /のディレクトリ変更

--- front/src/app/(public)/page.tsx
+++ front/src/app/(public)/page.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 
 interface FloatingCard {
   id: number;
