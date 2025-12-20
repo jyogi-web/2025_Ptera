c357ed163bdcd372560d5f036c0765b8d5973a7b
Yanai1005 <tatsuya871_1005@icloud.com>
2025-12-20 16:16:39 +0900

    タイトル

--- front/src/app/(authorized)/games/page.tsx
+++ front/src/app/(authorized)/games/page.tsx
@@ -85,7 +85,7 @@ export default function Games() {
               onClick={handleStart}
               style={styles.startButton}
             >
-              スタート
+              刹那のメンコ
             </button>
           ) : (
             <div style={styles.measuringBatch}>
