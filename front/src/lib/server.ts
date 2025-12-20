325a02336c092bb2423d45ab837cbeee8e2dcd5c
ibuki3268 <ryuku3268@gmail.com>
2025-12-20 13:44:18 +0900

    fix CI/CDを直したい

--- front/src/lib/server.ts
+++ front/src/lib/server.ts
@@ -10,10 +10,15 @@ function getAdminApp() {
     return apps[0];
   }
 
+  const serviceAccountKey = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
+  if (!serviceAccountKey) {
+    throw new Error("FIREBASE_SERVICE_ACCOUNT_KEY is not defined");
+  }
+
   return initializeApp({
     credential: cert(
       // 環境変数から認証情報を取得
-      JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string),
+      JSON.parse(serviceAccountKey),
     ),
   });
 }
